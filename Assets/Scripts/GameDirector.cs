38fe5196ae5acee238f35163bdbe5e258fa00ab1
RobotoSkunk <contact@robotoskunk.com>
2023-04-13 15:11:41 -0600

    Removed unnecessary packages

---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace RobotoSkunk.CircleBeats {
+
+	/// <summary>
+	/// Like a movie, the game director is the one who controls the flow of the game.
+	/// </summary>
+	public sealed class GameDirector : MonoBehaviour {
+		
+	}
+}
