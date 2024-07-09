eeb7188b693cb9fb87ce25b2d61380070b7d88d7
RobotoSkunk <contact@robotoskunk.com>
2024-07-08 20:12:21 -0600

    Added basic editor layout

---
+++ scripts/editor/Editor.cs
@@ -0,0 +1,38 @@
+/*
+	CircleBeats, a bullet hell rhythmic game.
+	Copyright (C) 2023 Edgar Lima (RobotoSkunk) <contact@robotoskunk.com>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+using Godot;
+
+
+public partial class Editor : Control
+{
+	[ExportGroup("UI Components")]
+	[ExportSubgroup("Split containers")]
+	[Export] HSplitContainer timelineHeader;
+	[Export] HSplitContainer timelineBody;
+
+
+	int lastVerticalScroll;
+
+
+	public override void _Process(double delta)
+	{
+		timelineHeader.SplitOffset = timelineBody.SplitOffset;
+	}
+}
